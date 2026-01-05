8fd5ab5fba2705c3760995add64dd0958e059872
KY888KAI <125953747+KY888KAI@users.noreply.github.com>
2026-01-05 16:34:24 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // 這裡引用了上面的 css
+import './index.css' // 引入樣式
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
